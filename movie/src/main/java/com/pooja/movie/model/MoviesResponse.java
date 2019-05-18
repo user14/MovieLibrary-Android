cbb3e9aec822e53221d69b622a9a8b8415dcc151
Poojai Rai <puji.rai@gmail.com>
2019-05-18 21:21:46 +0800

    MovieResponse class correct Movie import statement.
    MovieResponse class correct Movie import statement.

--- movie/src/main/java/com/pooja/movie/model/MoviesResponse.java
+++ movie/src/main/java/com/pooja/movie/model/MoviesResponse.java
@@ -1,8 +1,7 @@
 package com.pooja.movie.model;
-
-import com.example.tmdbmoview.model.Movie;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.pooja.movie.model.Movie;
 
 import java.util.List;
 
